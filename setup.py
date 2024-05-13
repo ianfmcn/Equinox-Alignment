514af0f5a5802f307507fc02d40baeb6296d4798
ianfmcn <114420968+ianfmcn@users.noreply.github.com>
2024-05-13 12:20:11 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
-
+setup(
+    name="equinox",
+    version=0.1,
+    description="CSE 185 Project",
+    author="Natsuki Romero, Leah Kim, Ian McNellis",
+    author_email=""
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "equinox=equinox.equinox:main"
+        ],
+    },
+)
