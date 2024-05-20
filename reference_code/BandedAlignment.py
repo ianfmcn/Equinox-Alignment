c270bf7099669a8f1573da44260819908902ed3d
l-j-kim <103155974+l-j-kim@users.noreply.github.com>
2024-05-20 13:01:40 -0700

    update BandedAlignment.py
    convert alignment type from global to local + debug issues

--- reference_code/BandedAlignment.py
+++ reference_code/BandedAlignment.py
@@ -1,68 +1,73 @@
 import sys
 import numpy as np
 
-def BandedAlignment(match_reward, mismatch_penalty, indel_penalty, band_parameter, s, t):
-    maxScore = 0
+def BandedAlignment(s, t, match_reward, mismatch_penalty, indel_penalty, band_parameter):
   
     n = len(s) + 1
     m = len(t) + 1
     score = np.zeros((n, m), dtype=int)
 
     for j in range(1, m):
-        score[0][j] = score[0][j - 1] - indel_penalty
-
+        score[0][j] = 0
     for i in range(1, n):
-        score[i][0] = score[i - 1][0] - indel_penalty
+        score[i][0] = 0
 
-    str1 = ""
-    str2 = ""
     down = 0
     side = 0
     diag = 0
     diff_down = 0
     diff_side = 0
+    max_score = 0
+    max_i = 0
+    max_j = 0
   
     for i in range(1, n):
         for j in range(1, m):
             diff_down = abs(i - 1 - j)
             if diff_down < band_parameter:
-                down = score[i - 1][j] - indel_penalty
+                down = score[i - 1][j] + indel_penalty
             else:
                 down = -10000
 
             diff_side = abs(i - (j - 1))
             if diff_side < band_parameter:
-                side = score[i][j - 1] - indel_penalty
+                side = score[i][j - 1] + indel_penalty
             else: 
                 side = -10000
             if s[i - 1] == t[j - 1]:
                 diag = score[i - 1][j - 1] + match_reward
             else: 
                 diag = score[i - 1][j - 1] + mismatch_penalty
-            score[i][j] = max(down, side, diag)
+            score[i][j] = max(down, side, diag, 0)
+            if max_score < score[i][j]:
+                max_score = score[i][j]
+                max_i = i
+                max_j = j
     
-    maxScore = score[n - 1][m - 1]
     for i in range(n - 1, 0, -1):
         for j in range(m - 1, 0, -1):
-            if score[i][j] == (score[i - 1][j] - indel_penalty):
+            if score[i][j] == (score[i - 1][j] + indel_penalty):
                 score[i][j] = -1  # down arrow
-            elif score[i][j] == (score[i][j - 1] - indel_penalty):
-                score[i][j] = 1  # side arrow (-->)
+            elif score[i][j] == (score[i][j - 1] + indel_penalty):
+                score[i][j] = 1  # side arrow 
             elif score[i][j] == (score[i - 1][j - 1] + match_reward):
-                score[i][j] = 0  # diagonal arrow
-            elif score[i][j] == (score[i - 1][j - 1] - mismatch_penalty):
-                score[i][j] = 0  # diagonal arrow
+                score[i][j] = 3  # diagonal arrow
+            elif score[i][j] == (score[i - 1][j - 1] + mismatch_penalty):
+                score[i][j] = 3  # diagonal arrow
 
     for j in range(1, m):
         score[0][j] = 1  # side
     for i in range(1, n):
         score[i][0] = -1  # down
 
-    str1 = ""  # s down
-    str2 = ""  # t across
-    i = n - 1
-    j = m - 1
-    while i != 0 or j != 0:
+  #start backtrace at max_i, max_j
+    str1 = ""  
+    str2 = "" 
+    i = max_i
+    j = max_j
+
+    stop = False
+    while stop == False:
         if score[i][j] == -1:  # down arrow (indel)
             str1 += s[i - 1]
             str2 += "-"
@@ -71,6 +76,8 @@ def BandedAlignment(match_reward, mismatch_penalty, indel_penalty, band_paramete
             str1 += "-"
             str2 += t[j - 1]
             j -= 1
+        elif score[i][j] == 0: #score of 0 in initial matrix
+            stop = True
         else:  # diagonal arrow (match/mismatch)
             str1 += s[i - 1]
             str2 += t[j - 1]
@@ -79,35 +86,18 @@ def BandedAlignment(match_reward, mismatch_penalty, indel_penalty, band_paramete
 
     rev1 = str1[::-1]
     rev2 = str2[::-1]
-
-    return maxScore, rev1, rev2
+    
+    print(max_score)
+    print(rev1)
+    print(rev2)
 
 #suggestion to use argparse instead of sys 
-if __name__ == "__main__":
-    #establish arguments
-    file_index = [idx for idx, s in enumerate(sys.argv) if '.fq' in s][0]
-    seq_file = sys.argv[file_index]
-    file = open(seq_file)
-    content = file.readlines()
-    #find two instances of '>'
-    #read next line
-    index = []
-    i = 0
-    for line in content:
-        l = line.strip()
-        if len(l) > 0 and l[0] == '>':
-            index.append(i+1)
-        i += 1
-    s = content[index[0]].strip()
-    t = content[index[1]].strip()
-    file.close()
+def main():
+    BandedAlignment("CATA", "CAT", 1, -1, -1, 3)
+    BandedAlignment("ACGTA", "ACT", 1, -1, -1, 3)
 
-    match = int(sys.argv[sys.argv.index("-m")+1])
-    mismatch = int(sys.argv[sys.argv.index("-s")+1])
-    indel = int(sys.argv[sys.argv.index("-d")+1])
-    band = int(sys.argv[sys.argv.index("-b")+1])
-        
-    alignment = BandedAlignment( match, mismatch, indel, band, s, t)
-    print(alignment)
+if __name__ == "__main__":
+    main()
+
 
 #python3 BandedAlignment.py s t -m match -s mismatch -d indel -b band
