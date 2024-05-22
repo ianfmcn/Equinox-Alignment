3b2d2411daa97ec3516f24a337ee2bd90ced192d
natsukiromero <103156146+natsukiromero@users.noreply.github.com>
2024-05-22 11:42:34 -0700

    Add files via upload

---
+++ reference_code/parsing.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+import argparse
+from locAL import main as locAL
+from BandedAlignment import main as BandedAL
+#from AffineAlignment import main as AffineAL
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="equinox",
+        description="Command-line script to perform alignment of FASTQ files"
+    )
+
+    # Input
+    parser.add_argument("txt", help="Txt file", type=str)
+    #for testing parsing, use .fa or .fastq
+    
+    # Output
+    parser.add_argument("-o", "--out", help="Write output to file. "\
+                "Default: stdout", metavar="FILE", type=str, required=False)
+    
+    # Alignment Penalties
+    parser.add_argument("-m", "--match-reward", \
+                help="match reward", \
+                type=int, metavar="INT",  required=True)
+    parser.add_argument("-s", "--mismatch-penalty", 
+                help="mismatch penalty", \
+                type=int, metavar="INT",  required=True)
+    parser.add_argument("-d", "--indel-penalty", \
+                help="indel penalty", \
+                type=int, metavar="INT",  required=True)
+    
+    # Optional Arguments
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument("-b", "--bandwidth", \
+                help="allowed bandwidth. ", \
+                type=int, metavar="INT",  required=False)
+    group.add_argument("-g", "--gap-open", 
+                help="gap open penalty", \
+                type=int, metavar="INT",  required=False)
+    group.add_argument("-e", "--gap-ext", \
+                help="gap extend penalty", \
+                type=int, metavar="INT",  required=False)
+    
+    
+    parser.add_argument("-k", "--min-seed-len", \
+                help="minimum seed length", \
+                type=int, metavar="INT",  required=False)
+    parser.add_argument("-c", "--max-occ", \
+                help="max-occ", \
+                type=int, metavar="INT",  required=False)
+    parser.add_argument("-R", "--RG-line", \
+                help="RG-line", \
+                type=int, metavar="INT",  required=False)
+    parser.add_argument("-t", "--cut-output", \
+                help="cut-output", \
+                type=int, metavar="INT",  required=False)
+    parser.add_argument("-C", "--comment-FAST", \
+                help="comment-FAST", \
+                type=int, metavar="INT",  required=False)
+    parser.add_argument("-v", "--verbose-level", \
+                help="verbose-level", \
+                type=int, metavar="INT",  required=False)
+    
+    args = parser.parse_args()
+    
+    align = locAL("GATA", "GA", args.match_reward, args.mismatch_penalty, args.indel_penalty, True)
+    print(align)
+
+
+if __name__ == "__main__":
+    main()
+'''
+if -b: run BandedAlignment
+if -g and -e: run Affine
+else: run locAL
+
+--> other options
+'''
+    
+'''
+#from local.py
+#establish arguments
+    file_index = [idx for idx, s in enumerate(sys.argv) if '.txt' in s][0]
+    seq_file = sys.argv[file_index]
+    file = open(seq_file)
+    content = file.readlines()
+    #find two instances of '>'
+    #read next line
+    index = []
+    i = 0
+    for line in content:
+        l = line.strip()
+        if len(l) > 0 and l[0] == '>':
+            index.append(i+1)
+        i += 1
+    s = content[index[0]].strip()
+    t = content[index[1]].strip()
+    file.close()
+
+    match = int(sys.argv[sys.argv.index("-m")+1])
+    mismatch = int(sys.argv[sys.argv.index("-s")+1])
+    indel = int(sys.argv[sys.argv.index("-d")+1])
+
+    align_command = [s for s in sys.argv if "-a" in s]
+    if len(align_command) == 0:
+        align = False
+    else:
+        align = True
+        
+    alignment = main(s, t, match, mismatch, indel, align)
+    print(alignment)
+'''
+
+#vim :set ff=unix :wq 
